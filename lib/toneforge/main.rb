bb831805b9a4249ffbb5fa9c8cef906d20cbcb81
rimmington <rimmington@gmail.com>
2010-06-30 13:57:08 +1000

    added GtkCurve

--- lib/toneforge/main.rb
+++ lib/toneforge/main.rb
@@ -7,9 +7,15 @@ module Toneforge
     builder = Gtk::Builder.new
     builder.add_from_file(Resources.find 'ui.glade')
     window = builder.get_object('wnd_main')
+    volume = builder.get_object('adj_volume')
+    amp_label = builder.get_object('lbl_amp')
     
     window.signal_connect("destroy") do
       Gtk.main_quit
+    end
+    
+    volume.signal_connect("value-changed") do
+      amp_label.set_text(format('%.1f%%', volume.value))
     end
 
     window.show_all
