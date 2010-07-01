24d8ef548d2d87d39f4b795d60b4cec91f131058
Aiden Nibali <dismaldenizen@gmail.com>
2010-07-01 11:17:35 +1000

    added handles and modelled wave around them

--- lib/toneforge/main.rb
+++ lib/toneforge/main.rb
@@ -6,13 +6,27 @@ module Toneforge
   SAMPLE_RATE = 8000
   DSP = File.open("/dev/dsp", "w")
   DSP.sync = true
-  WAVE = []
+  HANDLES = [[0.2, 0.7], [0.4, 0.3], [0.6, 0.7], [0.8, 0.3], [1.0, 0.7]]
 
   def self.beep(frequency, amplitude, duration)
     0.step(duration, 1.0 / SAMPLE_RATE) do |t|
       y = Math.sin(t * frequency) * 50 + 127;
       DSP.write(y.to_i.chr)
     end
+  end
+  
+  def self.get_y from_x
+    last_x, last_y = * HANDLES.last
+    result = 0
+    HANDLES.each do |x, y|
+      if from_x <= x
+        m = (y - last_y) / (x - last_x)
+        result = m * (from_x - last_x) + last_y
+        break
+      end
+      last_x, last_y = x, y
+    end
+    result
   end
   
   def self.main
@@ -28,13 +42,11 @@ module Toneforge
       Gtk.main_quit
     end
     
-    256.times do |i|
-      WAVE << 0.5 * (Math.sin(i.to_f / 5) + 1)
-    end
-    
     Thread.new do
       loop do
-        DSP.write(WAVE.map {|n| (n * 200).to_i.chr}.join)
+        0.step(1.0, 0.01) do |t|
+          DSP.write((get_y(t) * 200).to_i.chr)
+        end
       end
     end
 
@@ -44,9 +56,17 @@ module Toneforge
     
     drawing_area.signal_connect("expose-event") do
       cairo = drawing_area.window.create_cairo_context
-      cairo.move_to 0, WAVE.last * 200
-      WAVE.each_with_index do |y, x|
-        cairo.line_to x, y * 200
+      width = drawing_area.allocation.width
+      height = drawing_area.allocation.height
+
+      HANDLES.each do |x, y|
+        cairo.arc(x * width, y * height, 4, 0, Math::PI * 2)
+        cairo.stroke
+      end
+      
+      cairo.move_to 0, get_y(0)
+      0.step(1, 0.01) do |x|
+        cairo.line_to(x * width, get_y(x) * height)
       end
       cairo.stroke
     end
