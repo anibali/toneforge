19316d6e0b12f44b670eaf9af59a9e7cbc3e5235
Aiden Nibali <dismaldenizen@gmail.com>
2010-07-01 11:21:15 +1000

    added handles and modelled wave around them

--- lib/toneforge/main.rb
+++ lib/toneforge/main.rb
@@ -4,8 +4,8 @@ require 'toneforge/resources'
 
 module Toneforge
   SAMPLE_RATE = 8000
-  DSP = File.open("/dev/dsp", "w")
-  DSP.sync = true
+  #DSP = File.open("/dev/dsp", "w")
+  #DSP.sync = true
   HANDLES = [[0.2, 0.7], [0.4, 0.3], [0.6, 0.7], [0.8, 0.3], [1.0, 0.7]]
 
   def self.beep(frequency, amplitude, duration)
@@ -42,13 +42,13 @@ module Toneforge
       Gtk.main_quit
     end
     
-    Thread.new do
-      loop do
-        0.step(1.0, 0.01) do |t|
-          DSP.write((get_y(t) * 200).to_i.chr)
-        end
-      end
-    end
+    #Thread.new do
+    #  loop do
+    #    0.step(1.0, 0.01) do |t|
+    #      DSP.write((get_y(t) * 200).to_i.chr)
+    #    end
+    #  end
+    #end
 
     volume.signal_connect("value-changed") do
       amp_label.set_text('%.1f%%' % volume.value)
